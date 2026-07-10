f35d868a07d00eb713b368fee3a94f9ea4f354c4
Fedor Ihnatkevich <fedor.ihnatkevich@jetbrains.com>
2026-07-10 17:35:39 +0200

    [schema] Make `compose.experimental.hotReload` `jvm/app`-specific.
    Instead of just `jvm` specific that allows it to be used in `lib` modules.

--- sources/frontend-api/src/org/jetbrains/amper/frontend/schema/settings.kt
+++ sources/frontend-api/src/org/jetbrains/amper/frontend/schema/settings.kt
@@ -130,7 +130,7 @@ class ComposeResourcesSettings : SchemaNode() {
 
 class ComposeExperimentalSettings: SchemaNode() {
 
-    @PlatformSpecific(Platform.JVM) // we can only use Hot Reload on JVM for now, better warn users about it
+    @ProductTypeSpecific(ProductType.JVM_APP) // we can only use Hot Reload on JVM for now, better warn users about it
     @SchemaDoc("Experimental Compose hot-reload settings")
     val hotReload: ComposeExperimentalHotReloadSettings by nested()
 }
